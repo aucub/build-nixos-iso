c5ffb2cd091046c516bc0ded7fc8b6329d434ee5
dr56ekgbb <dr56ekgbb@mozmail.com>
2024-06-09 06:43:44 +0800

    ♻️ 重构(flake.nix)：使用nixos-cosmic模块，并添加缓存配置 🔧 配置(.github/workflows/build-iso.yml)：添加缓存配置，并更新构建脚本

--- flake.nix
+++ flake.nix
@@ -1,129 +1,96 @@
 {
   description = "NixOS installation media";
-  inputs.nixos.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixos.url = "nixpkgs/nixos-unstable";
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+  };
   outputs =
-    { self, nixos }:
+    {
+      self,
+      nixos-cosmic,
+      nixos,
+    }:
     {
       nixosConfigurations = {
         iso = nixos.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+            "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares.nix"
             (
               { pkgs, lib, ... }:
               {
-                isoImage.squashfsCompression = "zstd -Xcompression-level 22";
-                boot.plymouth.enable = lib.mkForce false;
-                boot.kernelPackages = pkgs.linuxPackages_latest;
-                boot.supportedFilesystems = lib.mkForce [
-                  "bcachefs"
-                  "btrfs"
-                  "reiserfs"
-                  "vfat"
-                  "f2fs"
-                  "xfs"
-                  "ntfs"
-                  "cifs"
-                ];
-                environment.gnome.excludePackages =
-                  (with pkgs; [
-                    loupe
-                    orca
-                    gnome-tecla
-                    gnome-tour
-                    gnome-photos
-                    gnome-menus
-                    baobab
-                    epiphany
-                    gnome-connections
-                    libsForQt5.qt5ct
-                    qt6Packages.qt6ct
-                    gnome-text-editor
-                  ])
-                  ++ (with pkgs.gnome; [
-                    gnome-contacts
-                    gnome-initial-setup
-                    yelp
-                    cheese
-                    gnome-music
-                    gnome-terminal
-                    gnome-font-viewer
-                    gnome-tweaks
-                    dconf-editor
-                    epiphany
-                    geary
-                    gnome-calendar
-                    gnome-calculator
-                    gnome-color-manager
-                    gnome-clocks
-                    gnome-characters
-                    gnome-maps
-                    gnome-weather
-                    gnome-software
-                    simple-scan
-                    totem
-                    tali
-                    iagno
-                    hitori
-                    atomix
-                    file-roller
-                    seahorse
-                  ]);
-                environment.variables = {
-                  EDITOR = "hx";
-                  QT_IM_MODULE = "fcitx";
-                  XMODIFIERS = "@im=fcitx";
-                  SDL_IM_MODULE = "fcitx";
-                  GLFW_IM_MODULE = "ibus";
-                };
-                environment.shells = with pkgs; [
-                  bashInteractive
-                  fish
-                ];
-                environment.systemPackages = with pkgs; [
-                  gnome-console
-                  bcachefs-tools
-                  btrfs-progs
-                  helix
-                  zed-editor
-                ];
-                programs.seahorse.enable = false;
-                programs.gnome-terminal.enable = false;
-                programs.file-roller.enable = false;
-                programs.command-not-found.enable = false;
-                programs.fish = {
-                  enable = true;
-                  interactiveShellInit = ''
-                    set -U fish_greeting
-                  '';
-                };
-                programs.neovim.enable = false;
-                services.gnome = {
-                  gnome-user-share.enable = false;
-                  gnome-online-accounts.enable = false;
-                  gnome-browser-connector.enable = false;
-                  gnome-initial-setup.enable = false;
-                  gnome-online-miners.enable = lib.mkForce false;
-                  games.enable = false;
-                  tracker.enable = false;
-                  tracker-miners.enable = false;
-                  rygel.enable = false;
-                  gnome-remote-desktop.enable = false;
-                  evolution-data-server.enable = lib.mkForce false;
-                };
-                services.avahi.enable = false;
-                services.xserver.excludePackages = with pkgs; [ xterm ];
-                services.kmscon = {
-                  enable = true;
-                  fonts = [
-                    {
-                      name = "Noto Sans CJK SC";
-                      package = pkgs.noto-fonts-cjk-sans;
-                    }
+                imports = [ nixos-cosmic.nixosModules.default ];
+                nix.settings = {
+                  substituters = [
+                    "https://cache.nixos.org/"
+                    "https://cosmic.cachix.org/"
                   ];
-                  extraOptions = "--term xterm-256color";
-                  extraConfig = "font-size=14";
-                  hwRender = true;
+                  trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                };
+                isoImage.squashfsCompression = "zstd -Xcompression-level 22";
+                boot = {
+                  plymouth.enable = lib.mkForce false;
+                  kernelPackages = pkgs.linuxPackages_latest;
+                  supportedFilesystems = lib.mkForce [
+                    "bcachefs"
+                    "btrfs"
+                    "reiserfs"
+                    "vfat"
+                    "f2fs"
+                    "xfs"
+                    "ntfs"
+                    "cifs"
+                  ];
+                };
+                environment = {
+                  variables = {
+                    EDITOR = "hx";
+                    QT_IM_MODULE = "fcitx";
+                    XMODIFIERS = "@im=fcitx";
+                    SDL_IM_MODULE = "fcitx";
+                    GLFW_IM_MODULE = "ibus";
+                  };
+                  shells = with pkgs; [
+                    bashInteractive
+                    fish
+                  ];
+                  systemPackages = with pkgs; [
+                    bcachefs-tools
+                    btrfs-progs
+                    helix
+                    zed-editor
+                  ];
+                };
+                programs = {
+                  xwayland.enable = false;
+                  command-not-found.enable = false;
+                  fish = {
+                    enable = true;
+                    interactiveShellInit = ''
+                      set -U fish_greeting
+                    '';
+                  };
+                  neovim.enable = false;
+                };
+                services = {
+                  desktopManager.cosmic.enable = true;
+                  xserver = {
+                    displayManager.gdm.enable = true;
+                    excludePackages = with pkgs; [ xterm ];
+                  };
+                  avahi.enable = false;
+                  kmscon = {
+                    enable = true;
+                    fonts = [
+                      {
+                        name = "Noto Sans CJK SC";
+                        package = pkgs.noto-fonts-cjk-sans;
+                      }
+                    ];
+                    extraOptions = "--term xterm-256color";
+                    extraConfig = "font-size=14";
+                    hwRender = true;
+                  };
                 };
                 fonts = {
                   enableDefaultPackages = false;
@@ -154,7 +121,10 @@
                 };
                 sound.enable = lib.mkForce false;
                 networking.firewall.enable = false;
-                hardware.enableAllFirmware = false;
+                hardware = {
+                  enableAllFirmware = false;
+                  pulseaudio.enable = lib.mkForce false;
+                };
                 documentation.enable = false;
                 xdg.mime.addedAssociations = {
                   "text/plain" = "dev.zed.Zed.desktop";
